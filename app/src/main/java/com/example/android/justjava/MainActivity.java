d1d11800204b5bd40c321168ec8ce7d59822f33a
Sabbir Siddiqui <siddiqus@live.com>
2018-08-09 02:40:20 +0600

    updates

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.android.justjava;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity {
+    int quantity = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,26 +17,32 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        display(1);
-        displayPrice(1);
+        setQuantity(quantity);
+        setPrice(quantity * 5);
     }
 
-    private void displayPrice(int i) {
-
+    private void setText(int id, String text) {
+        final TextView counterView = findViewById(id);
+        counterView.setText(text);
     }
 
-    private void display(int i) {
-        TextView counterView = (TextView) findViewById(R.id.counterValue);
-        counterView.setText("" + i);
+    private void setQuantity(int i) {
+        setText(R.id.counterValue, Integer.toString(i));
+    }
+
+    private void setPrice(int i) {
+        final String priceString = NumberFormat.getCurrencyInstance().format(i);
+        setText(R.id.priceValue, priceString);
     }
 
     public void increment(View view) {
-        int quantity = 3;
-        display(quantity);
+        quantity = quantity + 1;
+        setQuantity(quantity);
     }
 
     public void decrement(View view) {
-        int quantity = 1;
-        display(quantity);
+        if (quantity == 0) return;
+        quantity = quantity - 1;
+        setQuantity(quantity);
     }
 }
